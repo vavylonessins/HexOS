8eada85be8504b401c1d3311ac7f18c744ca01f3
vanochet <talismanchet@vk.com>
2023-07-27 19:03:38 +0300

    f

--- src/boot/bootcode/dictfs/boot.asm
+++ src/boot/bootcode/dictfs/boot.asm
@@ -19,65 +19,101 @@ start:
     push word 0 word 0
     pop ds es
     mov byte [BOOTDEV], dl
+
+    call _load_file_dictfs
+    jmp $
+
+_load_file_dictfs:
+    print "Required: "
+    mov si, word [_file_to_load]
+    call _printaz
+    print nl
+    ; load volume data
     mov ah, 2h
-    mov al, 1
+    mov al, 1h
     mov bx, word [dskbuf]
-    mov dh, 0
+    mov dh, 0h
     mov dl, byte [BOOTDEV]
-    mov ch, 0
-    mov cl, 3
-    push word 0
+    mov ch, 0h
+    mov cl, 3h
+    push word 0h
     pop es
     int 13h
-    jc errio
-    mov si, word [dskbuf]
+    print "FSINFO loaded to RAM", nl
+    jc ._errio
+    mov si, bx
 
-read_entry:
+._read_entry:
+    pusha
+    print "Analyzing entry...", nl
+    popa
+    ; entry is a file?
     cmp byte [si], 80h
-    jne find_next_entry
+    jne ._find_next_entry
 
-cmp_path:
+    pusha
+    print "It is a file", nl
+    popa
+
+    ; yes, it is a file, let's check if it is what we want
     add si, 0ah
-    mov di, loader_path
+    pusha
+    call _printaz
+    print nl
+    popa
+    mov di, word [_file_to_load]
     call cmpaz
-    jne find_next_entry
+    jnz ._find_next_entry
     sub si, 0ah
 
-load_file:
-    mov word [dskbuf], 2000h
+    pusha
+    print "It is required file", nl
+    popa
+
+    ; it is our file, let's load it
     mov ah, 2h
     mov al, byte [si+9h]
-    mov bx, 2000h
+    mov bx, word [_dskbuf_dest]
     mov cl, byte [si+1h]
     mov ch, byte [si+2h]
     mov dh, byte [si+3h]
     mov dl, byte [BOOTDEV]
-    push word 0
+    push word 0h
     pop es
     int 13h
-    jc badfs
+    jc ._badfs
     mov dl, [BOOTDEV]
-    jmp 0h:2000h
+    clc
+    ret
 
-find_next_entry:
+._find_next_entry:
+    pusha
+    print "It isn't required file", nl
+    ; this isn't our file, let's scan
+    ; fsinfo for next file entry
     add si, 0ah
     call endaz
     inc si
     cmp byte [si], 0h
-    je missingloader
-    jmp read_entry
+    ; it was last entry in fsinfo?
+    ; oops, file doesn't exist!
+    je ._missing
+    jmp ._read_entry
 
-errio:
+._errio:
     print "IO error", nl
-    jmp $
+    stc
+    ret
 
-badfs:
+._badfs:
     print "FS error", nl
-    jmp $
+    stc
+    ret
 
-missingloader:
-    print "/sys/hxldr.bin is missing", nl
-    jmp $
+._missing:
+    print "file is missing", nl
+    stc
+    ret
 
 
 _printaz:
@@ -91,12 +127,12 @@ _printaz:
 ._e:
     ret
 
-_lenaz:
+lenaz:
     lodsb
     test al, al
     jz ._e
     inc cx
-    jmp _lenaz
+    jmp lenaz
 ._e:
     ret
 
@@ -104,20 +140,24 @@ endaz:
     lodsb
     test al, al
     jz ._e
-    jmp _lenaz
+    jmp lenaz
 ._e:
     inc si
     ret
 
-; ASSUME: SI - asciiz 1, DI - asciiz 2
 cmpaz:
-    repnz cmpsb
+    xor cx, cx
+    push si
+    call lenaz
+    pop si
+    repz cmpsb
     ret
 
 BOOTDEV db ?
-spt db 12h
-hpc db 2
 dskbuf dw 100h
+_dskbuf_dest dw 2000h
+_file_to_load dw loader_path
+
 loader_path db "/sys/hxldr.bin", 0
 
 times 1feh-$+$$ db 0x00
